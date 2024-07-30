068a9f60c2e1408d224a365378d686ebde49242c
RomanL91 <dev_roman91@mail.ru>
2024-07-30 23:49:31 +0500

    выносим в настроки КОРСЫ + параметры старта

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,8 @@ from zoneinfo import ZoneInfo
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 BASE_DIR = Path(__file__).parent.parent
 
@@ -18,7 +20,25 @@ class SettingsAuth(BaseModel):
     algorithm: str = "RS256"
 
 
+class SettingsCORSMiddleware(BaseModel):
+    origins: list[str] = ["http://localhost", "http://localhost:3000"]
+    middleware: dict = {
+        "middleware_class": CORSMiddleware,
+        "allow_origins": origins,
+        "allow_credentials": True,
+        "allow_methods": ["*"],
+        "allow_headers": ["*"],
+    }
+
+
 class Settings(BaseSettings):
+    # == start app
+    app: str = "main:app"
+    host: str = "127.0.0.1"
+    port: int = 5001
+    reload_flag: bool = True
+
+    # == other
     api_v1_prefix: str = "/basket_api/v1"
     time_zone: ZoneInfo = ZoneInfo("Asia/Almaty")
 
@@ -26,6 +46,8 @@ class Settings(BaseSettings):
     db: SettingsDataBase = SettingsDataBase()
     # == Auth
     auth_jwt: SettingsAuth = SettingsAuth()
+    # == CORSMiddleware
+    middleware: SettingsCORSMiddleware = SettingsCORSMiddleware()
 
 
 settings = Settings()
---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+from core.settings import settings
+
+from api_v1 import router as router_v1
+
+
+app = FastAPI()
+
+app.add_middleware(**settings.middleware.middleware)
+
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+
+@app.get("/")
+async def start_test():
+    return {"message": "1, 2, 3 Start!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5001, reload=True)
