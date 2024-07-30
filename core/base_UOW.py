9a556805afba0ff4c0e8579b1608ecc3bdc2d52f
RomanL91 <dev_roman91@mail.ru>
2024-07-30 23:15:33 +0500

    UnitOfWork

---
+++ core/base_UOW.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+from core.db_manager import db_manager
+
+from basket_app.bascket_repository import BascketRepository
+
+
+class IUnitOfWork(ABC):
+
+    @abstractmethod
+    def __init__(self): ...
+
+    @abstractmethod
+    async def __aenter__(self): ...
+
+    @abstractmethod
+    async def __aexit__(self, *args): ...
+
+    @abstractmethod
+    async def commit(self): ...
+
+    @abstractmethod
+    async def rollback(self): ...
+
+
+class UnitOfWork:
+    def __init__(self):
+        self.session_factory = db_manager.get_scope_session()
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+        # для работы
+        self.bascket = BascketRepository(self.session)
+
+    async def __aexit__(self, *args):
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
