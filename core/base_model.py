773c7f5445d494c1c51fc2a1f7bbfea14d52b875
RomanL91 <dev_roman91@mail.ru>
2024-07-30 23:13:46 +0500

    базовая модель с установленными полями общими для всех остальных

---
+++ core/base_model.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+)
+
+from datetime import datetime
+
+from core import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(settings.time_zone),
+        nullable=True,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(settings.time_zone),
+        onupdate=lambda: datetime.now(settings.time_zone),
+        nullable=True,
+    )
