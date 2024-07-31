dbbec713a2c9e56282267f0281d528aad5dbb577
RomanL91 <dev_roman91@mail.ru>
2024-07-31 10:46:00 +0500

    добавление описаний к ендпоинтам + добавление обработки ошибок

--- api_v1/basket_api/views.py
+++ api_v1/basket_api/views.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Response, HTTPException, status
 
+from sqlalchemy.exc import NoResultFound
+
 # == My
 from basket_app import schemas
 from api_v1.basket_api.depends import UOF_Depends
@@ -14,6 +16,8 @@ router = APIRouter(tags=["Bascket"])
     "/",
     status_code=status.HTTP_201_CREATED,
     response_model=schemas.BasketPydantic | schemas.SimpleMSGErrorPydantic,
+    summary="Создание корзины.",
+    description="Создай корзину. Смотри пример тела. Из обязательно 'uuid_id'.",
 )
 async def create_bascket(
     new_bascket: schemas.BasketPydantic, uow: UOF_Depends, response: Response
@@ -32,6 +36,9 @@ async def create_bascket(
 @router.get(
     "/",
     response_model=list[schemas.BasketPydantic],
+    summary="Получение списка корзин.",
+    description="Можно получить список всех корзин. А нафиг? Пока пусть будет.",
+    deprecated=True,
 )
 async def get_basckets(uow: UOF_Depends):
     return await BascketService().get_baskets(uow)
@@ -40,31 +47,47 @@ async def get_basckets(uow: UOF_Depends):
 # GET           === === === === === === === ===
 @router.get(
     "/{uuid_id}/",
-    response_model=schemas.BasketPydantic,
+    response_model=schemas.BasketPydantic | schemas.SimpleMSGErrorPydantic,
+    summary="Получение экземпляра корзины по uuid_id.",
+    description="""Нужен uuid_id для получения экземпляра корзины. 
+                Вернет корзину с completed = False""",
 )
-async def get_bascket_by_uuid(uuid_id: str, uow: UOF_Depends):
-    return await BascketService().get_bascket_by_uuid(uow=uow, uuid_id=uuid_id)
+async def get_bascket_by_uuid(
+    uuid_id: str,
+    uow: UOF_Depends,
+):
+    try:
+        return await BascketService().get_bascket_by_uuid(uow=uow, uuid_id=uuid_id)
+    except NoResultFound as error:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Корзина с UUID {uuid_id!r} не найдена.",
+        )
 
 
 # UPDATE PUT    === === === === === === === ===
-@router.put(
-    "/{uuid_id}/",
-    response_model=schemas.BasketPydantic,
-)
-async def update_bascket(
-    uow: UOF_Depends,
-    uuid_id: str,
-    bascket_update: schemas.BasketPydantic,
-):
-    return await BascketService().update_bascket(
-        uow=uow, uuid_id=uuid_id, bascket_update=bascket_update
-    )
+# @router.put(
+#     "/{uuid_id}/",
+#     response_model=schemas.BasketPydantic,
+#     summary="Обновит всю корзину.",
+#     description="В теле запроса можно указать то поле, которое нужно обновить.",
+# )
+# async def update_bascket(
+#     uow: UOF_Depends,
+#     uuid_id: str,
+#     bascket_update: schemas.BasketPydantic,
+# ):
+#     return await BascketService().update_bascket(
+#         uow=uow, uuid_id=uuid_id, bascket_update=bascket_update
+#     )
 
 
 # UPDATE PATCH  === === === === === === === ===
 @router.patch(
     "/{uuid_id}/",
     response_model=schemas.BasketPydantic,
+    summary="Обновит поле корзины.",
+    description="В теле запроса можно указать то поле, которое нужно обновить.",
 )
 async def update_bascket(
     uow: UOF_Depends,
@@ -80,6 +103,8 @@ async def update_bascket(
 @router.delete(
     "/{uuid_id}/",
     status_code=status.HTTP_204_NO_CONTENT,
+    summary="Удали корзину.",
+    description="Удалит корзину безвозвратно.",
 )
 async def delete_bascket(uow: UOF_Depends, uuid_id: str) -> None:
     await BascketService().delete_bascket(uow=uow, uuid_id=uuid_id)
@@ -90,19 +115,21 @@ async def delete_bascket(uow: UOF_Depends, uuid_id: str) -> None:
     status_code=status.HTTP_200_OK,
     response_model=schemas.BasketPydantic | schemas.SimpleMSGErrorPydantic,
     summary="Создай или обнови корзину.",
-    description="Создаст новую корзину, если она не существует, или обновит существующию.",
-    responses={
-        # 422: {"description": "Да как так!"},
-        500: {"description": "Да пошел ты!"}
-    },
+    description="""Создаст новую корзину, если она не существует, 
+                или обновит существующию. Из обязательно 'uuid_id'.""",
+    responses={500: {"description": "Да пошел ты!"}},
     response_description="Информация о корзине.",
 )
 async def create_or_update_basket(
-    new_bascket: schemas.BasketPydantic, uow: UOF_Depends, response: Response,
+    new_bascket: schemas.BasketPydantic,
+    uow: UOF_Depends,
+    response: Response,
 ):
     try:
         uuid_id = new_bascket.uuid_id
-        return await BascketService().create_or_update_bascket(uow=uow, uuid_id=uuid_id, bascket_data=new_bascket)
+        return await BascketService().create_or_update_bascket(
+            uow=uow, uuid_id=uuid_id, bascket_data=new_bascket
+        )
     except HTTPException as error:
         response.status_code = error.status_code
         response_msg = schemas.SimpleMSGErrorPydantic(
