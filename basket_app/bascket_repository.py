a82b741975980ef16544f56003857fb002a4f310
RomanL91 <dev_roman91@mail.ru>
2024-08-08 03:08:19 +0500

    изменение метода репозитория корзины, вместо проверки перехват исключения NoResultFound

--- basket_app/bascket_repository.py
+++ basket_app/bascket_repository.py
@@ -1,17 +1,16 @@
 from basket_app.models import Basket
 from core.base_repository import SQLAlchemyRepository
+from sqlalchemy.exc import NoResultFound
 
 
 class BascketRepository(SQLAlchemyRepository):
     model = Basket
 
     async def create_or_update(self, uuid_id: str, data: dict):
-        # Пытаемся найти объект по UUID
-        existing_obj = await self.get_obj(uuid_id=uuid_id)
-
-        if existing_obj is None:
-            # Если объект не найден, создаем новый
-            return await self.create_obj(data)
-        else:
+        try:
+            # Пытаемся найти объект по UUID
+            existing_obj = await self.get_obj(uuid_id=uuid_id)
             # Если объект найден, обновляем его
             return await self.update_obj(uuid_id, data)
+        except NoResultFound as error:
+            return await self.create_obj(data)
