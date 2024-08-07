a91cb5f91d289d4e1a8289af75766cbe1462c84e
RomanL91 <dev_roman91@mail.ru>
2024-08-08 03:14:04 +0500

    добавлен метод в репозиторий OrderRepository для аннотирования ордера даннными корзины

--- order_app/order_repository.py
+++ order_app/order_repository.py
@@ -1,6 +1,18 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
 from order_app.models import Order
 from core.base_repository import SQLAlchemyRepository
 
 
 class OrderRepository(SQLAlchemyRepository):
     model = Order
+
+    async def get_info_order_with_basket(self, uuid_id: str):
+        stmt = (
+            select(Order)
+            .options(joinedload(Order.basket))  # Загрузка связанных данных корзины
+            .filter(Order.uuid_id == uuid_id)
+        )
+        res = await self.session.execute(stmt)
+        return res.scalar_one()
