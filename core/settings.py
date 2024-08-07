0b516f680106fb4c6dde2bb99085a817cd4c5fbb
RomanL91 <dev_roman91@mail.ru>
2024-08-08 03:21:53 +0500

    настройки для подключения к БД + для подключения к АПИ магазина

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,13 @@ BASE_DIR = Path(__file__).parent.parent
 
 
 class SettingsDataBase(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    # for example "postgresql://user:password@localhost/dbname"
+    # url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    url: str = (
+        "postgresql+asyncpg://MyBasketUser:MyBasketPassword@localhost:5433/MyBasketDataBase"
+    )
     echo: bool = True  # Для дебага
+    future: bool = True
 
 
 class SettingsAuth(BaseModel):
@@ -32,6 +37,27 @@ class SettingsCORSMiddleware(BaseModel):
     }
 
 
+class SettingsApiShop(BaseModel):
+    host_addr_api: str = "http://127.0.0.1:8000/"
+    prod_by_ids_template: str = "{host}api/v1/products/by_ids/{prod_ids_str}/"
+    url_admin_prod_detail: str = "{host}admin/app_products/products/{prod_id}/change/"
+    url_api_prod_detail: str = "{host}api/v1/products/{prod_slug}/"
+
+    def _format_url(self, template: str, **kwargs) -> str:
+        defaults = {"host": self.host_addr_api}
+        data = {**defaults, **kwargs}
+        return template.format(**data)
+
+    def get_prod_by_ids(self, **kwargs):
+        return self._format_url(self.prod_by_ids_template, **kwargs)
+
+    def get_url_admin_prod_detail(self, **kwargs):
+        return self._format_url(self.url_admin_prod_detail, **kwargs)
+
+    def get_url_api_prod_detail(self, **kwargs):
+        return self._format_url(self.url_api_prod_detail, **kwargs)
+
+
 class Settings(BaseSettings):
     # == start app
     app: str = "main:app"
@@ -49,6 +75,8 @@ class Settings(BaseSettings):
     auth_jwt: SettingsAuth = SettingsAuth()
     # == CORSMiddleware
     middleware: SettingsCORSMiddleware = SettingsCORSMiddleware()
+    # == ApiShop
+    api_shop: SettingsApiShop = SettingsApiShop()
 
 
 settings = Settings()
