feff05c729ed9b6d4b86a2c1bc1c6ae547589fad
RomanL91 <dev_roman91@mail.ru>
2024-08-13 03:04:09 +0500

    зависимости fastapi-pagination

--- api_v1/order_api/depends.py
+++ api_v1/order_api/depends.py
@@ -1,7 +1,9 @@
-from typing import Annotated
 from fastapi import Depends
+from typing import Annotated
+from fastapi_pagination import Params
 
 from core.base_UOW import IUnitOfWork, UnitOfWork
 
 
 UOF_Depends = Annotated[IUnitOfWork, Depends(UnitOfWork)]
+Params_Depends = Annotated[Params, Depends(Params)]
