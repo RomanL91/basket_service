94d9cd7427e82e4cde282bba5050826afc2fa373
RomanL91 <dev_roman91@mail.ru>
2024-08-20 01:19:02 +0500

    рефакторинг схем pydantic

--- order_app/schemas.py
+++ order_app/schemas.py
@@ -14,11 +14,14 @@ class OrderStatusType(str, Enum):
     NEW = "NEW"
     INWORK = "INWORK"
     COMPLITED = "COMPLITED"
+    CANCELED = "CANCELED"
 
 
 # Pydantic модель
 class OrderPydantic(BaseModel):
-    model_config = ConfigDict(strict=True)
+    model_config = ConfigDict(
+        strict=True,
+    )
 
     uuid_id: Annotated[
         str,
@@ -27,7 +30,7 @@ class OrderPydantic(BaseModel):
             description="Уникальный идентификатор заказа, наследуется от корзины.",
             examples=["fcff9649-c7cc-498c-8ee2-c84785a68521"],
         ),
-    ]
+    ] = None
     order_status: Annotated[
         OrderStatusType | str,
         Field(
@@ -43,7 +46,7 @@ class OrderPydantic(BaseModel):
             description="Номер телефона для контакта.",
             examples=["+77714658976"],
         ),
-    ]
+    ] = None
     comment: Annotated[
         str | None,
         Field(
@@ -53,7 +56,7 @@ class OrderPydantic(BaseModel):
                 "Очень жду доставку. Если что, вот мой другой номер - +77782346754."
             ],
         ),
-    ]
+    ] = None
     delivery_type: Annotated[
         DeliveryType | str,
         Field(
@@ -69,7 +72,7 @@ class OrderPydantic(BaseModel):
             description="Город отгрузки (важно: выбор делает софт, не пользователь).",
             examples=["Астана"],
         ),
-    ]
+    ] = None
     delivery_address: Annotated[
         str | None,
         Field(
@@ -77,20 +80,63 @@ class OrderPydantic(BaseModel):
             description="Адрес доставки (указывает пользователь, форма свободная).",
             examples=["Улица Достык, 8/4."],
         ),
-    ]
-    # created_at: Annotated[
-    #     datetime | None,
-    #     Field(
-    #         ...,
-    #         description="Время создания ордера в системе.",
-    #         examples=["2024-08-07T23:54:51.628882"],
-    #     ),
-    # ]
-    # updated_at: Annotated[
-    #     datetime | None,
-    #     Field(
-    #         ...,
-    #         description="Время обновления ордера в системе.",
-    #         examples=["2024-08-07T23:54:51.628882"],
-    #     ),
-    # ]
+    ] = None
+    manager_executive: Annotated[
+        str | None,
+        Field(
+            ...,
+            description="Менежер, принявщий заявку.",
+            examples=["Иванов Иван"],
+        ),
+    ] = None
+    manager_mailbox: Annotated[
+        str | None,
+        Field(
+            ...,
+            description="Почтовый ящик менеджера.",
+            examples=["Ivanov@example.com"],
+        ),
+    ] = None
+    created_at: Annotated[
+        datetime | None,
+        Field(
+            ...,
+            description="Время создания ордера в системе.",
+            examples=["2024-08-07T23:54:51.628882"],
+        ),
+    ] = None
+    updated_at: Annotated[
+        datetime | None,
+        Field(
+            ...,
+            description="Время обновления ордера в системе.",
+            examples=["2024-08-07T23:54:51.628882"],
+        ),
+    ] = None
+
+
+class CreateOrderPydantic(OrderPydantic):
+    model_config = ConfigDict(
+        strict=True,
+        json_schema_extra={
+            "example": {
+                "uuid_id": "fcff9649-c7cc-498c-8ee2-c84785a68521",
+                "phone_number": "+77714658976",
+                "shipping_city": "Астана",
+                "delivery_address": "Улица Достык, 8/4.",
+            }
+        },
+    )
+
+    uuid_id: str
+    phone_number: str
+    shipping_city: str
+    delivery_address: str
+
+
+class ReadOrderPydantic(OrderPydantic):
+    pass
+
+
+class UpdateOrderPydantic(OrderPydantic):
+    pass
