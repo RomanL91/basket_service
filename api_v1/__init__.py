885d2192daf2a9e68c77d12aab2abf094195c901
RomanL91 <dev_roman91@mail.ru>
2024-12-04 01:53:36 +0500

    подключение роутера для представления платежек

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .basket_api.views import router as bascket_router
 from .order_api.views import router as order_router
+from .payment_api.view import router as payment_router
 
 
 router = APIRouter()
 
 router.include_router(router=bascket_router, prefix="/bascket")
 router.include_router(router=order_router, prefix="/order")
+router.include_router(router=payment_router, prefix="/payment")
