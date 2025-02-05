00577ae4b3878319cdcd8913559ffd422903c27f
RomanL91 <dev_roman91@mail.ru>
2025-02-05 20:31:31 +0500

    переработка логика высчита общей суммы заявки

--- core/base_utils.py
+++ core/base_utils.py
@@ -1,6 +1,10 @@
+import jwt
+
+import httpx
+
 from decimal import Decimal
 
-import jwt
+from collections import Counter
 
 from core.settings import settings
 
@@ -22,12 +26,26 @@ def check_token(jwt_value: str) -> bool:
 def get_total_sum_per_basket(shipping_city: str, basket_items: list) -> Decimal:
     total_amount_to_order = []
     try:
-        for el in basket_items:
-            count = el.get("count", 0)
-            price = Decimal(el.get("prod").get("price").get(shipping_city))
+        prod_with_count = dict(Counter(basket_items))
+        params = {"ids": ",".join(map(str, basket_items)), "city": shipping_city}
+        url = settings.api_shop.get_prod_by_ids()
+        response = httpx.get(url, params=params)
+        response.raise_for_status()
+        response_data = response.json()
+        products = response_data.get("results", None)
+
+        if products is None:
+            raise ValueError("В данном городе нет этого товара.")
+        for el in products:
+            count = prod_with_count.get(el["id"], 0)
+            price = Decimal(el.get("stocks").get(shipping_city).get("price"))
             total_amount_to_pos = price * count
             total_amount_to_order.append(total_amount_to_pos)
         total_sum = sum(total_amount_to_order)
+
+        # return Decimal(100)
         return Decimal(total_sum)
-    except (KeyError, TypeError):
+    except (KeyError, TypeError) as e:
         raise ValueError("Нарушение структуры корзины.")
+    except httpx.ConnectError:
+        raise ValueError("Нет ответа от сервиса.")
