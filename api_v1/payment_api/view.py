dd18d993f28c6c237dd3a5164e9cc0bb8a4aa3c1
RomanL91 <dev_roman91@mail.ru>
2025-02-08 14:47:59 +0500

    endpoint для принятия обратного вызова

--- api_v1/payment_api/view.py
+++ api_v1/payment_api/view.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, status
 from order_app.order_service import OrdertService
 from api_v1.order_api.depends import UOF_Depends
 from order_app.schemas import (
-    TransactionPaymentSchema,
+    BankCallbackModel,
 )
 
 
@@ -16,14 +16,14 @@ router = APIRouter(tags=["Payment"])
     response_model=None,
     summary="endpoint для обратного вызова от банка",
     description="""
-    После совершения оплаты с формы оплаты от банка, последний 
+    После совершения оплаты с формы оплаты от банка, последний
     совершает обратный вызов на данный endpoint.
     Создается запись о результате платежа и закреплятся за ордером,
     переводя статус последнего в состояние - оплачен.
     """,
 )
-async def test_payment(
-    new_payment: TransactionPaymentSchema,
+async def payment(
+    new_payment: BankCallbackModel,
     uow: UOF_Depends,
 ):
     await OrdertService().accepting_payment(uow=uow, new_payment=new_payment)
